aa29f14ca3f4b667c829ed5c038cd44b965421a3
FikretCansel <fikretcansel25@hotmail.com>
2022-06-19 18:31:23 +0300

    fist

---
+++ helloword-server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+console.log(process.env.channel)
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
